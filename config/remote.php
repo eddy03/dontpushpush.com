43d7a7b2967ba1b8851cb6dfcae095673bb3bb6f
eddy <eddytech03@gmail.com>
2015-12-17 21:51:10 +0800

    predefine key in the remote.php config

--- config/remote.php
+++ config/remote.php
@@ -32,7 +32,7 @@ return [
             'username'  => env('SSH_USERNAME', ''),
             'password'  => env('SSH_PASSWORD', ''),
             'key'       => env('SSH_KEY', ''),
-            'keytext'   => env('SSH_KEYTEXT', ''),
+            'keytext'   => env('SSH_KEYTEXT', 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAglBMcj8XBauOXzE185pvbrRGaADqWiMGiTT0dT2NVhAu6e59fPfSETmL9Qa4Vvx21ZgizszZnN9cYZiSWhuz5a69l2hYPZQ2hsS3Un3V00ot1kFa4BMDnZzDXRvSf2fqYzeeeRbQxVxAzrtkRf8TKH6h5xxsSdayxl0yJ6BB5bcLn+oWBKXPhqPd9IJiYkwCHB1+wvHXr4FE6MIdlsJu1y7WH/8QBBz2PqadFu60uWvh5HhxCidSKqDvR5aHh6/LJEx2T+dfPulnUcD3Ha63xnyCB09JpMHEP91yZ2vcFkqYfIBbngCYN2yrxs5MzhZMzYC5p5WWr4rHrPyXKLTQWQ== rsa-key-20150907'),
             'keyphrase' => '',
             'agent'     => '',
         ],
